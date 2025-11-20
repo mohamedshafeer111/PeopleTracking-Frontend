e225d46b4ec9876433f20104f3df1f9b792c613e
Pbala2002 <pbalakrishnan18082002@gmail.com>
2025-11-20 18:34:02 +0530

    last

--- src/app/components/service/websocket/websocket.ts
+++ src/app/components/service/websocket/websocket.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
+import { Subject } from 'rxjs';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Websocket {
+
+
+  private socket$!: WebSocketSubject<any>; // ✅ fixed
+  private zoneCountSubject = new Subject<any>();
+  zoneCount$ = this.zoneCountSubject.asObservable();
+
+  constructor(private ngZone: NgZone) {
+    this.connect();
+  }
+
+  private connect() {
+    this.socket$ = webSocket('ws://165.22.215.89:5202/ws/ZoneCount');
+
+    this.socket$.subscribe({
+      next: (message) => {
+        this.ngZone.run(() => {
+          this.zoneCountSubject.next(message);
+        });
+      },
+      error: (err) => console.error('❌ WebSocket Error:', err),
+      complete: () => console.warn('⚠️ WebSocket closed')
+    });
+  }
+
+  disconnect() {
+    this.socket$?.complete();
+  }
   
 }
