a51aac8aa767a06ac8a44639f133288aa9aee9ec
mohamedshafeer111 <mohamedshafeersha@gmail.com>
2026-03-24 11:59:58 +0530

    Updated backend UR

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@
 
 export const environment = {
   production: true,
-     apiUrl: 'https://acres-interval-reuters-simon.trycloudflare.com/api/',
+     apiUrl: ' https://double-impact-poultry-quantities.trycloudflare.com/api/',
   wsUrl: 'wss://arkansas-modern-clarke-bookmarks.trycloudflare.com/ws/ZoneCount'
 };
 
