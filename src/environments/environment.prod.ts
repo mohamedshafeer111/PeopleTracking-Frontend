5acda8308b1abdd854285ee9a955b0351ac664cf
mohamedshafeer111 <mohamedshafeersha@gmail.com>
2026-05-26 11:39:00 +0530

    production build updated with tunnel url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 
-export const environment = {
-  production: true,
-     apiUrl: 'http://172.16.100.29:5402/api/',
-       wsBaseUrl: 'ws://172.16.100.29:5402',
+// export const environment = {
+//   production: true,
+//      apiUrl: 'http://172.16.100.29:5402/api/',
+//        wsBaseUrl: 'ws://172.16.100.29:5402',
 
-  // wsUrl: 'ws://165.22.215.89:5202/ws/ZoneCount'
-   //   wsUrl: '/ws/ZoneCount'
-};
+//   // wsUrl: 'ws://165.22.215.89:5202/ws/ZoneCount'
+//    //   wsUrl: '/ws/ZoneCount'
+// };
 
 
 // export const environment = {
@@ -15,3 +15,10 @@ export const environment = {
 //   wsUrl: 'wss://phcc.purpleiq.ai/ws/ZoneCount', 
 //   imageBaseUrl: 'https://phcc.purpleiq.ai/api/uploads/' 
 // };
+export const environment = {
+  production: true,
+  apiUrl: 'https://pays-demonstrates-unix-taking.trycloudflare.com/api/',
+  wsUrl: 'wss://pays-demonstrates-unix-taking.trycloudflare.com/ws/ZoneCount',
+    wsBaseUrl: 'wss://pays-demonstrates-unix-taking.trycloudflare.com', //
+  imageBaseUrl: 'https://pays-demonstrates-unix-taking.trycloudflare.com/api/uploads/'
+};
